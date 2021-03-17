51a311f38ba6b530cb8cbd706b2e1111d57287a4
Divya Vijendra Girase <girase.di@husky.neu.edu>
2021-03-17 16:51:12 -0400

    Added changes

--- Bookings.py
+++ Bookings.py
@@ -2,23 +2,24 @@ from flask import Flask, request, jsonify
 from bson.objectid import ObjectId
 import uuid
 import DatabaseOperations as database
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
-
-@app.route('/book-trip', methods=["GET"])
+@app.route('/book-trip', methods=["POST"])
 def bookTrip():
-    firstName = "Divya"#request.json["firstName"]
-    lastName = "Girase"#request.json["lastName"]
-    source = "Boston"#request.json["source"]
-    destination = "NewYork" #request.json["destination"]
-    journeyDate = "02-21-2021" #request.json["date"]
+    firstName = request.json["firstNameP"]
+    lastName = request.json["lastNameP"]
+    source = request.json["sourceP"]
+    destination = request.json["destinationP"]
+    journeyDate = request.json["journeydDateP"]
 
     booking = dict(firstName=firstName, lastName=lastName, source=source,
                 destination=destination, journeyDate=journeyDate,
                 _id=str(ObjectId()))
-    database.add_booking(booking)
-    return "Booking new trip"
+    #database.add_booking(booking)
+    return jsonify("Booked new trip")
 
 @app.route('/bookings', methods=["GET"])
 def getAllBookings():
